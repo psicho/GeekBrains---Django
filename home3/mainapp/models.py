50b9f0a98298617661ad2abf95f763f6523b34e1
psicho <9400878@gmail.com>
2017-04-19 10:21:20 +0400

    Закончено второе ДЗ и начато третье ДЗ. Добавлены первые записи в БД.

--- home3/mainapp/models.py
+++ home3/mainapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Work(models.Model):
+    organization = models.CharField(verbose_name='Организация', max_length=32)
+    region = models.CharField(verbose_name='Регион', max_length=32, blank=True)
+    site = models.CharField(verbose_name='Сайт', max_length=64, blank=True)
+    position = models.CharField(verbose_name='Должность', max_length=16)
+    duties = models.TextField(verbose_name='Обязанности')
+    period = models.PositiveIntegerField(verbose_name='Время работы', default=1)
+
+class Teach(models.Model):
+    universitet = models.CharField(verbose_name='Университет', max_length=32)
+    specials = models.CharField(verbose_name='Специализация', max_length=32, blank=True)
+    site = models.CharField(verbose_name='Сайт', max_length=64, blank=True)
+    datestart = models.DateField(verbose_name='Дата поступления')
+    dateend = models.DateField(verbose_name='Дата окончания')
