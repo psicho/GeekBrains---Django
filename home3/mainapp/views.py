b5395c09e8a3d768644b9333fa1711a7a3a615e6
psicho <9400878@gmail.com>
2017-04-19 12:20:30 +0400

    Закончено второе ДЗ и начато третье ДЗ. Добавлены первые записи в БД.

--- home3/mainapp/views.py
+++ home3/mainapp/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, render_to_response
+from mainapp.models import Teach
+from mainapp.models import Work
 
 def main(request):
     return render_to_response("index.html")
 
 def work(request):
-    return render_to_response("work.html")
+    place_of_work = Work.objects.all()
+    return render_to_response("work.html", {'organization':[work.organization for work in place_of_work]})
 
 def teach(request):
-    return render_to_response("teach.html")
+    place_of_teach = Teach.objects.all()
+    return render_to_response("teach.html",
+                              {'universitet':[teach.universitet for teach in place_of_teach]})
