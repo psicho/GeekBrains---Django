59f1189e850e73d2dfd478bfe6b436021d7667aa
psicho <9400878@gmail.com>
2017-04-19 16:44:01 +0400

    Добавлен вывод мест работы и учебы из БД.

--- home3/mainapp/views.py
+++ home3/mainapp/views.py
@@ -7,9 +7,15 @@ def main(request):
 
 def work(request):
     place_of_work = Work.objects.all()
-    return render_to_response("work.html", {'organization':[work.organization for work in place_of_work]})
+    return render_to_response("work.html", {'organization':[work.organization for work in place_of_work],
+                                            'position': [work.position for work in place_of_work],
+                                            'site': [work.site for work in place_of_work]})
 
 def teach(request):
     place_of_teach = Teach.objects.all()
     return render_to_response("teach.html",
-                              {'universitet':[teach.universitet for teach in place_of_teach]})
+                              {'universitet':[teach.universitet for teach in place_of_teach],
+                               'datestart':[teach.datestart for teach in place_of_teach],
+                               'dateend': [teach.dateend for teach in place_of_teach],
+                               'specials': [teach.specials for teach in place_of_teach],
+                               'site': [teach.site for teach in place_of_teach]})
